8ec5bb9f00c4ea1b1452ba458811df9e35f36066
lstahlinski <lclindbe@asu.edu>
2021-03-22 13:11:10 -0700

    Fix defect # 7 & Remove switch statement

--- src/main/java/Stats.java
+++ src/main/java/Stats.java
@@ -11,28 +11,30 @@ public class Stats {
     }
 
     public Stats(Mascotmon.Name name) {
-        switch (name) {
-            case ALBERT:
-                attack = 60;
-                defense = 40;
-                health = 100;
-                break;
-            case RALPHIE:
-                attack = 30;
-                defense = 65;
-                health = 105;
-                break;
-            case SPARKY:
-                attack = 70;
-                defense = 40;
-                health = 90;
-                break;
-            case BULLY:
-                attack = 40;
-                defense = 40;
-                health = 110;
-                break;
+        if (name == Mascotmon.Name.ALBERT){
+            attack = 60;
+            defense = 40;
+            health = 100;
         }
-
+        else if (name == Mascotmon.Name.RALPHIE){
+            attack = 30;
+            defense = 65;
+            health = 105;
+        }
+        else if (name == Mascotmon.Name.SPARKY){
+            attack = 70;
+            defense = 40;
+            health = 90;
+        }
+        else if (name == Mascotmon.Name.SPARKY){
+            attack = 40;
+            defense = 40;
+            health = 110;
+        }
+        else {
+            attack = 50;
+            defense = 50;
+            health = 100;
+        }
     }
 }
