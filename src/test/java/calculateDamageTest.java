f6ffa0436c19c31ff8a5b82e37706c30c44345ca
lstahlinski <lclindbe@asu.edu>
2021-03-22 14:04:35 -0700

    Create calculatedDamage.java class

--- src/test/java/calculateDamageTest.java
+++ src/test/java/calculateDamageTest.java
@@ -8,17 +8,15 @@ import org.junit.runners.Parameterized.Parameters;
 import java.lang.reflect.Constructor;
 import java.util.Arrays;
 import java.util.Collection;
-
-//import main.java.BearWorkshop;
-
 import static org.junit.Assert.*;
 
 @RunWith(Parameterized.class)
-public class GivenBlackBox {
+public class calculateDamageTest {
+    
     private Class<BattleScenario> classUnderTest;
 
     @SuppressWarnings("unchecked")
-    public GivenBlackBox(Object classUnderTest) {
+    public calculateDamageTest(Object classUnderTest) {
         this.classUnderTest = (Class<BattleScenario>) classUnderTest;
     }
 
@@ -35,12 +33,13 @@ public class GivenBlackBox {
         return Arrays.asList(classes);
     }
 
+
+
     private BattleScenario createBattleScenario(Mascotmon a, Mascotmon d) throws Exception {
         Constructor<BattleScenario> constructor = classUnderTest.getConstructor(Mascotmon.class, Mascotmon.class);
         System.out.println(constructor);
         return constructor.newInstance(a, d);
-    }
-
+    } 
     
     @Before
     public void setUp() throws Exception {
@@ -49,56 +48,6 @@ public class GivenBlackBox {
 
     @After
     public void tearDown() throws Exception {
-    }
-
-    // sample test
-
-    /**
-     * Battle between Bully and Ralphie on a sunny day with ground attack
-     * @throws Exception 
-     */
-    @Test
-    public void BvsRSunnyGround() throws Exception {
-        
-        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.BULLY);
-        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.RALPHIE);
-        
-        BattleScenario fight1 = createBattleScenario(attacker1, defender1); 
-        System.out.println("    BvsRSunnyGround");
-        
-        fight1.setEnvironment(Environment.Weather.sunny);
-        Attack attack = new Attack(80, "Ground");
-        
-        //Calculation: 80 * 1 * 1 - 65 * 1 *1 == 15
-        // 80 put into attack manually, no weather bonuses on either side, Ralphi has 65 defense
-        
-        double damage = fight1.calculateDamage(attack, attacker1, defender1);
-        System.out.println("         Damage dealt: " + damage);
-        assertEquals(damage, 15, 0.2);
-    }
-    
-    /**
-     * Battle between Bully and Ralphie on a sunny day with ground attack
-     * @throws Exception 
-     */
-    @Test
-    public void RvsBSunnyGround() throws Exception {
-        
-        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.RALPHIE);
-        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.BULLY);
-        
-        BattleScenario fight1 = createBattleScenario(attacker1, defender1); 
-        System.out.println("    RvsBSunnyGround");
-        
-        fight1.setEnvironment(Environment.Weather.sunny);
-        Attack attack = new Attack(80, "Ground");
-        
-        //Calculation: 80 * 1.2 * 1 * 1 - 40 * 1 *1
-        // 80 put into attack manually, Ralhpie gets bonus for ground attack no weather bonuses or type bonus
-        
-        double damage = fight1.calculateDamage(attack, attacker1, defender1);
-        System.out.println("         Damage dealt: " + damage);
-        assertEquals(damage, 56, 0.2);
     }
 
     /**
@@ -347,5 +296,4 @@ public class GivenBlackBox {
         System.out.println("         Damage dealt: " + damage);
         assertEquals(damage, 22, 0.2);
     }
-
 }
